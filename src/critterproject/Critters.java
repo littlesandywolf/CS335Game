e568a748e04ba8409e650702d149d4cf182eb570
sophieandersonn <sophie.anderson@simmons.edu>
2026-02-12 10:35:01 -0500

    Add Critters class

---
+++ src/critterproject/Critters.java
@@ -0,0 +1,40 @@
+package critterproject;
+
+public class Critters {
+	private static int nextID = 1; //global counter
+	protected int id;
+	protected String name; //critter name
+	protected boolean playerOwned; //boolean whether or not owned by player
+	
+	protected Stats run, swim, climb, fly, stamina;
+	
+	public Critters(String name, boolean playerOwned, int[] genes) {
+		this.id = nextID++;
+		this.name = name;
+		this.playerOwned = playerOwned;
+		
+		//genes paired up: runA, runB, swimA, swimB, etc.
+		this.run = new Stats("Run", genes[0], genes[1]);
+        this.swim = new Stats("Swim", genes[2], genes[3]);
+        this.climb = new Stats("Climb", genes[4], genes[5]);
+        this.fly = new Stats("Fly", genes[6], genes[7]);
+        this.stamina = new Stats("Stamina", genes[8], genes[9]);
+	}
+	
+	//method to check if the id from two different critters are equal
+	public boolean isSameCritter(Critters other) {
+        return this.id == other.id;
+	}
+	
+	//getter for stats -> instead of if/else used switch statement so we can add later (https://www.w3schools.com/java/java_switch.asp) 
+	public Stats getStat(String statName) {
+        return switch (statName.toLowerCase()) {
+            case "run" -> run;
+            case "swim" -> swim;
+            case "climb" -> climb;
+            case "fly" -> fly;
+            case "stamina" -> stamina;
+            default -> null;
+        };
+    }
+}
