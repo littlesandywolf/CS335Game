29a1d6e2a3e2e35c899d3034145c3dfb548e8a0b
sophieandersonn <sophie.anderson@simmons.edu>
2026-02-12 10:53:59 -0500

    Add Main class

--- src/critterproject/Main.java
+++ src/critterproject/Main.java
@@ -1,5 +1,18 @@
 package critterproject;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Welcome to The Critter Game");
+        System.out.print("Name your new critter: ");
+        String name = s.nextLine();
+
+        //creating random genes (= 50 points total) here?
+
+        System.out.println("Created: " + name);
+        //"get stat" print statement here
+	}
 }
