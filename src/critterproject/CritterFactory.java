2b23b4ff374b18cecccf91beb51f6a3b5fb48275
gigi0318 <2giagill@gmail.com>
2026-02-15 09:52:26 +0530

    Update CritterFactory.java

--- src/critterproject/CritterFactory.java
+++ src/critterproject/CritterFactory.java
@@ -1,5 +1,19 @@
 package critterproject;
-
+import java.util.Random;
 public class CritterFactory {
-	//gene splitter method will go in here 
+	private static final Random rand = new Random();
+
+    public static int[] generateGenes() {
+        int[] genes = new int[10];
+        int remaining = 50;
+
+        while (remaining > 0) {
+            int idx = rand.nextInt(10);
+            if (genes[idx] < 10) {
+                genes[idx]++;
+                remaining--;
+            }
+        }
+        return genes;
+    }
 }
