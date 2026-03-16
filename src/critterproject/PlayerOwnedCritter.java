a1bd37c721710af7aa3bb37b57d42d7bd3c0eb88
gigi0318 <2giagill@gmail.com>
2026-03-16 20:06:52 +0530

    Update PlayerOwnedCritter.java

--- src/critterproject/PlayerOwnedCritter.java
+++ src/critterproject/PlayerOwnedCritter.java
@@ -3,27 +3,50 @@ package critterproject;
 import java.util.HashSet;
 import java.util.Set;
 
+enum LifeStage {
+    BABY, JUVENILE, ADULT, ELDER
+}
+
 public class PlayerOwnedCritter extends Critters {
     private int age = 0;
     private int bond = 0;
-
+    private LifeStage lifeStage;
+    
     // Prevents duplicate names for player-owned critters
     private static final Set<String> usedNames = new HashSet<>();
 
     public PlayerOwnedCritter(String name, int[] genes) {
-        super(name, true, genes);
-        usedNames.add(name); // assume unique on creation (main only creates one for now)
+        String finalName = name;
+        int suffix = 1;
+        while (usedNames.contains(finalName)) {
+            finalName = name + " #" + suffix++;
+        }
+        super(finalName, true, genes);
+        usedNames.add(finalName);
+        this.lifeStage = LifeStage.BABY;
     }
 
     // Age & Bond
     public void incrementAge() {
         age++;
+        updateLifeStage();
     }
 
     public void addBond(int amount) {
         bond = Math.min(100, bond + amount); // optional cap
     }
 
+    public void updateLifeStage() {
+        if (age < 5) {
+            lifeStage = LifeStage.BABY;
+        } else if (age < 15) {
+            lifeStage = LifeStage.JUVENILE;
+        } else if (age < 30) {
+            lifeStage = LifeStage.ADULT;
+        } else {
+            lifeStage = LifeStage.ELDER;
+        }
+    }
     // Rename with duplicate protection
     public boolean rename(String newName) {
         if (newName.equals(this.name)) return true;
@@ -38,7 +61,7 @@ public class PlayerOwnedCritter extends Critters {
 
     public int getAge() { return age; }
     public int getBond() { return bond; }
-
+    public LifeStage getLifeStage() { return lifeStage; }
     public void displayStats() {
         System.out.println(name + " (Age: " + age + " Bond: " + bond + ")");
         System.out.println(run);
